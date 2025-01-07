abe6d171e87420622a5fd0d1d1cd04506ffdc2b6
haidersiddique <siddiquehaider674@gmail.com>
2025-01-07 09:31:14 -0800

    some update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
   <nav className='flex top-0 w-[90%] z-50 sticky justify-start flex-col pl-4 bg-[#024555] text-[#d9cc51] '>
           <div>
-          <h1 className='text-[#d9cc51] text-2xl flex justify-start'> Daniel Martinez</h1>
+          <h1 className='text-[#d9cc51] text-2xl flex justify-start'> Haider Siddique</h1>
           <ul className='text-white flex z-50 gap-8 pt-2 pl-4 pb-3 lg:text-[16px] md:text-[14px] sm:text-[12px] text-[10px] font-thin'>
           <Link to="/"><li>Home</li></Link>
               <Link to="/about"><li>about</li></Link>
