074df4776afd15a0d10c19026618cca0ff651e9b
haidersiddique <siddiquehaider674@gmail.com>
2025-01-10 11:08:00 -0800

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,15 +16,15 @@ const About = () => {
         <h1  data-aos="zoom-out-down" data-aos-duration="2000" className='text-6xl text-[#d9cc51] font-bold'>About Me</h1>
         <h1 data-aos="fade-down"
      data-aos-easing="linear"
-     data-aos-duration="8000" className='font-thin text-[18px] pt-11 text-white'>
-I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click <br /> me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like <br /> on your page. I’m a great place for you to tell a story and let your users know a little more about you.
-<br />
-<br />
-<br />
-This is a great space to write a long text about your company and your services. You can use this space to go <br /> into a little more detail about your company. Talk about your team and what services you provide. Tell your <br /> visitors the story of how you came up with the idea for your business and what makes you different from your <br /> competitors. Make your company stand out and show your visitors who you are.
+     data-aos-duration="8000" className='font-medium text-[19px] pt-11 text-white'>
+     I'm a versatile programmer passionate about designing websites for all kinds of<br />
+     projects. Whether it's e-commerce, blogs, portfolios, or anything in between, I<br />
+     create functional, user-friendly sites tailored to your needs. With a strong <br />
+     focus on clean code and responsive design, I bring ideas to life on the web,<br />
+     no matter the subject.
         </h1>
     </div>
-    <div className='flex justify-center pt-24'>
+    <div className='flex justify-center pt-16'>
         <h1  data-aos="zoom-out-up" data-aos-duration="2000" className='text-3xl text-[#d9cc51] font-serif pb-20'>Thank You</h1>
     </div>
     <hr />
